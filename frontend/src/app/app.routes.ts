849b56421b11e551a6a4c033bf81517f36956ef6
BautiBustos <bautibustos10@gmail.com>
2025-08-17 18:59:35 -0300

    update: app routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,26 @@
+// app.routes.ts // 
 import { Routes } from '@angular/router';
 import { DashAdm } from './dash-adm/dash-adm';
 
-export const routes: Routes = [
-    {path:'dashboard', component: DashAdm},
-    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
-    // aca agregar ubicacion de error :D
+// Componentes del dashboard
+import { Empresa } from './dash-adm/empresa/empresa';
+import { Usuario} from './dash-adm/usuario/usuario';
+import { Centrales} from './dash-adm/centrales/centrales';
 
+export const routes: Routes = [
+
+  // Ruta del dashboard, que contiene las rutas anidadas
+  {
+    path: 'dashboard',
+    component: DashAdm,
+    children: [
+      { path: '', redirectTo: 'empresa', pathMatch: 'full' },
+      { path: 'empresa', component: Empresa },
+      { path: 'usuario', component: Usuario },
+      { path: 'centrales', component: Centrales },
+    ],
+  },
+  
+  // Ruta comodín para manejar URLs no encontradas
+  { path: '**', redirectTo: '' },
 ];
