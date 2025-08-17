6cf0b2bc7a89d46d99f3c2cf42ee036fd7787280
BautiBustos <bautibustos10@gmail.com>
2025-08-17 18:56:34 -0300

    feat: se añadieron los 'componentes centrales, empresas, usuarios' y arrreglaron conflicos con rutas

--- frontend/src/app/dash-adm/dash-adm.ts
+++ frontend/src/app/dash-adm/dash-adm.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router'; // 1. Importa RouterOutlet
 import { Nav } from './nav/nav'; // La ruta correcta es aquí
 
 @Component({
   selector: 'app-dash-adm',
   standalone: true,
-  imports: [Nav],
+  imports: [RouterOutlet, Nav],
   templateUrl: './dash-adm.html',
   styleUrl: './dash-adm.css'
 })
