fd4301d6cd3772746725c419f01d3039d6fe860f
BautiBustos <bautibustos10@gmail.com>
2025-08-17 15:47:37 -0300

    feat: añade colores en syles, se suma la fuente, se inicio el dashboard administrador

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashAdm } from './dash-adm/dash-adm';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'dashboard', component: DashAdm},
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+    // aca agregar ubicacion de error :D
+
+];
