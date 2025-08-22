d0415c665f20faa476ddf43d3b7cc5fc217cc3b7
Heyme <heyme.coraspe@gmail.com>
2025-08-21 22:10:43 -0300

    feat: registro en angular

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { DashAdm } from './dash-adm/dash-adm';
+import { FormularioRegistro } from './auth/formulario-registro/formulario-registro';
+
+
+
 
 export const routes: Routes = [
     {path:'dashboard', component: DashAdm},
-    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
-    // aca agregar ubicacion de error :D
+    {path: '', component: FormularioRegistro},
+    {path: 'inicio', component: FormularioRegistro},
+    {path: '', redirectTo: '/inicio', pathMatch: 'full'},
 
 ];
