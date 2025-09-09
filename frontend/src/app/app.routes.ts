d38e6388aad18abfd0d792ec8b9236bca18b142c
Franco <francoavila9356@gmail.com>
2025-09-09 18:06:56 -0300

    Eliminacion de Carpetas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,37 +1,24 @@
-// app.routes.ts // 
 import { Routes } from '@angular/router';
-
 import { QuienesSomosComponent } from './quienes-somos/quienes-somos.component';
 import { FormularioRegistro } from './auth/formulario-registro/formulario-registro';
 import { HomeView } from './home_view/home_view';
 import { Ingreso } from './auth/ingreso/ingreso';
-
-// Componentes del dashboard de administrador
 import { DashAdm } from './dash-adm/dash-adm';
 import { EmpresaComponent } from './dash-adm/empresa/empresa';
 import { UsuarioComponent } from './dash-adm/usuario/usuario';
 import { CentralesComponent } from './dash-adm/centrales/centrales';
-// Componentes del dashboard de usuario
 import { DashUser } from './dash-user/dash-user';
 import { Metricas } from './components/metricas/metricas';
 import { Historial } from './components/historial/historial';
 import { Alertas } from './components/alertas/alertas';
-
-// componente de ruta no encontrada
 import { Error404Component } from './error404/error404.component';
-
 
 export const routes: Routes = [
   { path: '', component: HomeView, title: 'Home' },
   { path: 'home', component: HomeView, title: 'Home' },
-    // ruta para tu componente 'quienes-somos'
   { path: 'quienes-somos', component: QuienesSomosComponent },
-
-  //{path: '', component: FormularioRegistro},
   { path: 'sing-up', component: FormularioRegistro },
   { path: 'login', component: Ingreso },
-
-  // Ruta del dashboard administrador
   {
     path: 'dash-admin',
     component: DashAdm,
@@ -42,7 +29,6 @@ export const routes: Routes = [
       { path: 'centrales', component: CentralesComponent },
     ],
   },
-  //dashboard usuario 
   {
     path: 'dash-user',
     component: DashUser,
@@ -51,9 +37,7 @@ export const routes: Routes = [
       { path: 'temperatura', component: Metricas },
       { path: 'historial', component: Historial },
       { path: 'alertas', component: Alertas },
-    ]
+    ],
   },
-  // Ruta comodín para manejar URLs no encontradas
   { path: '**', component: Error404Component }
-
 ];
