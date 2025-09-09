db928b390b6e21fa36280a0c9f928a3929f36b96
Heyme <heyme.coraspe@gmail.com>
2025-09-09 19:51:54 -0300

    feat: se agrega cuit y sus validaciones. fix: se corrije warning de control de flujo

--- frontend/src/app/auth/formulario-registro/formulario-registro.ts
+++ frontend/src/app/auth/formulario-registro/formulario-registro.ts
@@ -10,6 +10,7 @@ import { Router } from '@angular/router';
   templateUrl: './formulario-registro.html',
   styleUrl: './formulario-registro.css'
 })
+
 export class FormularioRegistro {
 formularioRegistro: FormGroup;
 
@@ -18,23 +19,56 @@ formularioRegistro: FormGroup;
     nombre: new FormControl('', Validators.required),
     apellido: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
-    contrasenia: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{5,12}$'),Validators.minLength(5), Validators.maxLength(12) ])
-
+    contrasenia: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{5,12}$') ]),
+    confirmarContrasenia: new FormControl('', [Validators.required,]),
+    cuit: new  FormControl('', [Validators.required, Validators.pattern('^[0-9]{11}$')]),
 });
 }
 /* se mueven los get del html al typescript */
-get contrasenia() {
-  return this.formularioRegistro.get('contrasenia');
+get nombre() {
+  return this.formularioRegistro.get('nombre');
+}
+
+get apellido() {
+  return this.formularioRegistro.get('apellido');
+}
+
+get cuit() {
+  return this.formularioRegistro.get('cuit');
 }
 
 get email() {
   return this.formularioRegistro.get('email');
 }
+/**gets mostrar invalidos */
+get mostrarCuitInvalido(){
+  return this.cuit?.invalid && this.cuit?.touched;
+}
 
+get mostrarEmailIvalido(){
+  return this.email?.invalid && this.email?.touched;
+}
+
+get contrasenia() {
+  return this.formularioRegistro.get('contrasenia');
+}
+
+get confirmarContrasenia(){
+  return this.formularioRegistro.get('confirmarContrasenia');
+}
+
+get mostrarContraseniaInvalida(){
+  return this.contrasenia?.invalid && this.contrasenia?.touched;
+}
+
+get contraseniasNoCoinciden(){
+  return this.contrasenia?.value !== this.confirmarContrasenia?.value && this.confirmarContrasenia?.touched;
+}
+
+/**Eventos */
 inicio() {
   this.router.navigate(['/']);
 }
-
 onSubmit() {
   console.log('El formulario es válido:', this.formularioRegistro.valid);
   console.log('Valores del formulario:', this.formularioRegistro.value);
