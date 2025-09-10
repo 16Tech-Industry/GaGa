5c7d2908d6efca6923f671b7cf764f049ccdc7b4
BautiBustos <bautibustos10@gmail.com>
2025-09-10 20:52:12 -0300

    si

---
+++ frontend/src/app/dash-adm/modal-abm/modal-abm.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-abm',
+  imports: [],
+  templateUrl: './modal-abm.html',
+  styleUrl: './modal-abm.css'
+})
+export class ModalAbm {
+
+}
