b0cc2bbd405da419f128b4ef75b524e3e3dd1785
cryptoempirearg <socialmediaenterpises@gmail.com>
2025-09-14 20:52:45 -0300

    Login

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,17 +2,6 @@ import { Component, signal } from '@angular/core';
 import { Router, RouterOutlet, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { AuthService } from './services/auth';
-import { HomeView } from './home_view/home_view';
-import { Ingreso } from './auth/ingreso/ingreso';
-import { DashAdm } from './dash-adm/dash-adm';
-import { FormularioRegistro } from './auth/formulario-registro/formulario-registro';
-
-
 
 @Component({
   selector: 'app-root',
@@ -27,18 +16,3 @@ export class App {
 
   constructor(public router: Router) {}
 }
-
-@NgModule({
-  declarations: [
-    Ingreso,
-  ],
-  imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-  ],
-  providers: [
-    AuthService
-  ],
-})
-export class AppModule { }
