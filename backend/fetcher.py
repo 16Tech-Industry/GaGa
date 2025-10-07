31be67dc7b38c9a5846dd9257636dee09a8aa49f
BautiBustos <bautibustos10@gmail.com>
2025-10-07 20:27:28 -0300

    update: funcionando localmente el insertar metricas. Se esta añadiendo la funcion para agregar empresas

--- backend/fetcher.py
+++ backend/fetcher.py
@@ -1,5 +1,4 @@
 import psycopg2, requests, datetime, random
-
 # establece datos para la conexion
 conector =psycopg2.connect(
             host="localhost",
@@ -21,25 +20,26 @@ def get_metricas():
     '''
     url = '''https://api.openweathermap.org/data/2.5/weather?units=metric&lat=-31.4082913&lon=-64.1959429&appid=d0713df127f4895e41f66a21d8a4783b'''
     response = requests.get(url)
+    # covierte la data en un formato json
     data = response.json()
+    #mejoramos el orden  de la respuesta de la api para el futuro incert 
     data_estructurada = {
-    'temperatura' : data['main']['temp'],
-    'humedad' : data['main']['humidity'],
-    'viento' : data['wind']['speed'],
-    'fecha' : datetime.datetime.now(),
-    # valor de simulacion de una empresa mediana
-    'watts' : random.randrange(5000, 9999),
-    # valor basado en el consumo de una fabrica textil (Litros*Dia)
-    'litros' : random.randrange(2500, 3000),
-    'id_central': random.randrange(1, 3)
+        'temperatura' : data['main']['temp'],
+        'humedad' : data['main']['humidity'],
+        'viento' : data['wind']['speed'],
+        'fecha' : datetime.datetime.now(),
+        # valor de simulacion de una empresa mediana
+        'watts' : random.randrange(5000, 9999),
+        # valor basado en el consumo de una fabrica textil (Litros*Dia)
+        'litros' : random.randrange(2500, 3000),
+        'id_central': random.randrange(1, 3)
     }
-    print(data_estructurada)
     return data_estructurada
 
 def insert_metricas(valores):
     '''
     ## Inserta metricas en la base de datos
-    
+    Dentro de la tabla metricas se hace el insert de los valores obtenidos o generados de forma al azar 
     '''
     insert = (
         valores['fecha'],
@@ -50,25 +50,27 @@ def insert_metricas(valores):
         valores['watts'],
         valores['id_central']
     )
-    print(insert)
     cur.execute('''insert into gaga."METRICAS"
             (id_metrica,fecha,
             temperatura,humedad, viento, 
             litros_consumidos, watt_consumidos, 
             centrales_id_central) 
             values (default, %s, %s, %s, %s, %s, %s, %s);''',insert)
+    
+    #se confirma los cambios
     conector.commit()
-"""
-cur.execute(f"SET search_path TO gaga, public;")
 
-cur.execute('insert into gaga."EMPRESAS" (email, nombre_empresa, direccion, cuit ) values (%s, %s, %s, %s);',
-            ("Empresa@empresa","lamus mu", "Calle Falsa 123", "5551234"))
+def nueva_empresa(values):
+    '''
+    ## Inserta una nueva empresa en la base de datos
+    Inserta una nueva empresa en la base de datos
+    '''
+    cur.execute('insert into gaga."EMPRESAS" (email, nombre_empresa, direccion, cuit ) values (%s, %s, %s, %s);', values)
+    conector.commit()
 
-cur.execute('SELECT * FROM gaga."EMPRESAS";')
-rows = cur.fetchall()
-"""
-valores = get_metricas()
-insert_metricas(valores)
+# se obtienen los calores de la metrica y se los pasan al insert
+if __name__ == "__main__":
+    insert_metricas(get_metricas())
+    cur.close()
+    conector.close()
 
-cur.close()
-conector.close()
