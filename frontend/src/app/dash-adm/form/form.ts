c0e515eb5d30ac174af43b9c1ba75410924ec12c
BautiBustos <bautibustos10@gmail.com>
2025-10-08 00:57:02 -0300

    doc: se documentaron vistas de administrador

--- frontend/src/app/dash-adm/form/form.ts
+++ frontend/src/app/dash-adm/form/form.ts
@@ -3,17 +3,17 @@ import { ChangeDetectionStrategy, Component, inject, model } from '@angular/core
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {
-  MAT_DIALOG_DATA,
-  MatDialogActions,
-  MatDialogClose,
-  MatDialogContent,
-  MatDialogRef,
-  MatDialogTitle,
+ MAT_DIALOG_DATA, // Token de inyección para pasar datos AL diálogo.
+  MatDialogActions, // Contenedor para las acciones del diálogo (botones).
+  MatDialogClose, // Directiva para un botón que cierra el diálogo.
+  MatDialogContent, // Contenedor para el contenido principal del diálogo.
+  MatDialogRef, // Referencia al diálogo abierto, para poder controlarlo (ej: cerrarlo).
+  MatDialogTitle, // Contenedor para el título del diálogo.
 } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { User } from '@app/models/User';
 
+// Define la estructura de los datos que este diálogo espera recibir.
 export interface DialogData {
   nombre: string;
   apellido: string;
@@ -22,10 +22,10 @@ export interface DialogData {
 }
 
 @Component({
-  selector: 'form',
-  templateUrl: 'form.html',
-  standalone: true,
-  imports: [
+  selector: 'form', // Selector HTML para usar este componente.
+  templateUrl: 'form.html', // Ruta al archivo HTML de la vista del formulario.
+  standalone: true, // Indica que es un componente autocontenido.
+  imports: [ // Lista de módulos y componentes necesarios para la plantilla (form.html).
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
@@ -38,14 +38,19 @@ export interface DialogData {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DialogOverviewExampleDialog {
+  // Inyecta las dependencias necesarias para manejar el diálogo y los datos.
   readonly dialogRef = inject(MatDialogRef<DialogOverviewExampleDialog>);
+  // recibe la configuracion inicial del dialogo, sean datos o la est5ructura vacia
   readonly data = inject<DialogData>(MAT_DIALOG_DATA);
 
+  // modelos de datos enlazados a los campos del formulario en la vista (form.html)
   readonly nombre = model(this.data.nombre);
   readonly apellido = model(this.data.apellido);
   readonly email = model(this.data.email);
   readonly cuitEmpresa = model(this.data.cuitEmpresa);
 
+  //Método que se llama cuando se hace clic en el botón de cancelar/cerrar.
+  //    Cierra el diálogo sin devolver ningún dato.
   onNoClick(): void {
     this.dialogRef.close();
   }
--- frontend/src/app/services/abm-admin.ts
+++ frontend/src/app/services/abm-admin.ts
@@ -9,27 +9,35 @@ import { User } from '@app/models/User';
   providedIn: 'root'
 })
 export class UserService {
+  // url de conexion a servidor
   url = "http://localhost:3000/usuarios";
 
+  // Inyecta el cliente HTTP de Angular para poder hacer peticiones web.
   constructor(private http: HttpClient) {}
 
   // POST: Agrega un nuevo usuario
+  // El omit<User, 'id'> indica que el objeto user no debe incluir la propiedad 'id' al crear un nuevo usuario.
+  // teniendo en cuenta que este ultimo es incremental y se añade automaticamente en el backend
   createUser(user: Omit<User, 'id'>): Observable<User> {
+    // el return es un observable que emite un "aviso" que el usuario fue creado
     return this.http.post<User>(this.url, user);
   }
 
-  // GET: Obtiene todos los usuarios
+  // GET: Obtiene todos los usuarios de la base de datos
   getUsers(): Observable<User[]> {
+    // el return es un observable que emite un array de usuarios
     return this.http.get<User[]>(this.url);
   }
 
   // PUT: Actualiza un usuario existente
   updateUser(user: User): Observable<User> {
+    // se "construye" una url para la actualizacion del usuario en la base de datos.
     return this.http.put<User>(`${this.url}/${user.id}`, user);
   }
 
   // DELETE: Elimina un usuario del servidor
   deleteUser(id: number): Observable<any> {
+    // se "construye" una url para la eliminacion del usuario en la base de datos.
     return this.http.delete(`${this.url}/${id}`);
   }
 }
